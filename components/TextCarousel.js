078b127232d3799f88ce72220eff047d941c2358
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2022-11-18 22:55:36 -0500

    services page changes

--- components/TextCarousel.js
+++ components/TextCarousel.js
@@ -5,15 +5,55 @@ export default function TextCarousel() {
   return (
     <div className={styles.wrapper}>
       <ul>
-        <li>Columbia Art Museum</li>
-        <li>University of South Carolina</li>
-        <li>Dick Dyer Mercades</li>
-        <li>701 Whaley</li>
-        <li>Farm2Table</li>
-        <li>Segra Park</li>
-        <li>Wren</li>
-        <li>Jim Hudson Automodive</li>
-        <li>Flock and Rally</li>
+        <li className={styles.left}>
+          <a target="_blank" rel="null" href="https://www.columbiamuseum.org/">
+            Columbia Art Museum
+          </a>
+        </li>
+        <li className={styles.right}>
+          <a target="_blank" rel="null" href="https://sc.edu/">
+            University of South Carolina
+          </a>
+        </li>
+        <li className={styles.left}>
+          <a
+            target="_blank"
+            rel="null"
+            href="https://www.dickdyermercedes.com/"
+          >
+            Dick Dyer Mercedes
+          </a>
+        </li>
+        <li className={styles.right}>
+          <a target="_blank" rel="null" href="https://701whaley.com/">
+            701 Whaley
+          </a>
+        </li>
+        <li className={styles.left}>
+          <a target="_blank" rel="null" href="https://www.f2tproductions.com/">
+            Farm2Table
+          </a>
+        </li>
+        <li className={styles.right}>
+          <a target="_blank" rel="null" href="https://segrapark.com/">
+            Segra Park
+          </a>
+        </li>
+        <li className={styles.left}>
+          <a target="_blank" rel="null" href="https://www.scwren.org/">
+            Wren
+          </a>
+        </li>
+        <li className={styles.right}>
+          <a target="_blank" rel="null" href="https://www.scwren.org/">
+            Jim Hudson Automodive
+          </a>
+        </li>
+        <li className={styles.left}>
+          <a target="_blank" rel="null" href="https://www.flockandrally.com/">
+            Flock and Rally
+          </a>
+        </li>
       </ul>
     </div>
   );
