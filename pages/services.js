1e58f0871725ce194216248b0f193e6875f67746
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2022-11-21 15:07:04 -0500

    styling changes to services page

--- pages/services.js
+++ pages/services.js
@@ -7,11 +7,12 @@ export default function Services() {
   return (
     <div className={styles.page}>
       <section style={{ paddingBottom: "0" }}>
-        <h3 className={styles["img-header"]}>
-          Currently Serving <span>|</span> Affiliated With
-        </h3>
+        <h3 className={styles["img-header"]}>Currently Serving</h3>
+        <div className="divider margin" />
         <div className={styles["zoom-wrapper"]}>
           <Zoom />
+          <h3 className={styles["text-header"]}>Frequent Events</h3>
+          <div className="divider margin" />
           <TextCarousel />
         </div>
       </section>
